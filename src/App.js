2a408b26349f790c463428c2cb06da95bffa3d74
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-04-22 15:31:40 +0100

    new modifs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import "./App.css";
 import Link from "./Links/Link";
 import Logo from "./Logo/Logo";
 
+/*
+function Logo() {
+  return <img src="/images/Logo-Simplon.png" alt="" />;
+}
+
+function Link() {
+  return (
+    <span className="link">
+      <a href="#" id="first">
+        Accueil
+      </a>
+      <a href="#">Equipe</a>
+      <a href="#">A propos</a>
+      <a href="#">Contact</a>
+    </span>
+  );
+}
+*/
+
 function App() {
   return (
     <div className="App">
--- src/Links/Link.js
+++ src/Links/Link.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "./Link.css";
 
+/*
+const list = ["Accueil", "Equipe", "A propos", "Contact"];
+
 function Link() {
   return (
     <span className="link">
+      {list.map((el) => (
+        <a href="#">{el}</a>
+      ))}
+    </span>
+  );
+}
+*/
+
+function Link() {
+  return (
+    <div className="link">
       <a href="#" id="first">
         Accueil
       </a>
       <a href="#">Equipe</a>
       <a href="#">A propos</a>
       <a href="#">Contact</a>
-    </span>
+    </div>
   );
 }
 
